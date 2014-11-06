02499edf3cc45a895e188567a5422e4be03be325
Aruz Parajuli <arujparajuli@gmail.com>
2014-11-06 23:07:23 +0530

    User Modle + Secure Password

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,15 +1,14 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "layout links" do
-  	get root_path
-  	assert_template 'static_pages/home'
-  	assert_template "a[href=?]", root_path, count: 2
-  	assert_template "a[href=?]", help_path,
-  	assert_template "a[href=?]", contact_path,
-  	assert_template "a[href=?]", about_path 
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end
+
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-  	@user = User.new(name:"Aruj Parajuli", email:"arujparajuli@gmail.com")
+  	@user = User.new(name:"Aruj Parajuli", email:"arujparajuli@gmail.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -41,6 +41,11 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should have minimum length" do
+  	@user.password = @user.password_confirmation = "a" * 5
+  	assert_not @user.valid?
   end
 end
 
