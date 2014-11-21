285ac7a193107fa1505c393e634ef5092b77c6bd
Aruz Parajuli <arujparajuli@gmail.com>
2014-11-21 19:28:41 +0530

    User Session

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "Login with invalid information" do
+  	get login_path
+  	assert_template 'session/new'
+  	post login_path, session: {email:"",password:""}
+  	assert_template 'session/new'
+  	assert_not flash.empty?
+  	get root_path
+  	assert flash.empty?
+  end
+end
